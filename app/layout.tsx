ae2c2d38c3efd944d8534df4cb0fd75daef358ae
AltifyDev <altifygenerator@gmail.com>
2026-04-01 06:20:08 -0500

    google meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
-import type { Metadata } from "next";
 import "./globals.css";
-export const metadata = {
- 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
   title: {
     default: "Little Cove Early Learning | Mount Ida, Arkansas Daycare",
     template: "%s | Little Cove Early Learning",
   },
   description:
     "Faith-based daycare in Mount Ida, Arkansas providing safe, nurturing care for infants, toddlers, and preschoolers.",
+
+  verification: {
+    google: "2BwurFuZChwt31SjViuSzFaqkjDO3wz5lQSDVxZGVos",
+  },
 };
-<meta name="google-site-verification" content="2BwurFuZChwt31SjVIuSzFaqkjDO3wz5lQSDVxZGVos" />
+
 export default function RootLayout({
   children,
 }: Readonly<{
